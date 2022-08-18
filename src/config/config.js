fccd8efdeb1aa08d8ba28d4d72123b8c570d4bc3
ybramhankar <45839817+ybramhankar@users.noreply.github.com>
2022-08-19 00:07:08 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
-export const API_KEY = '9fceff62ed261f7fcdce42a6ff1df1bb';
+export const API_KEY = 'api key here';
 export const BASE_URL = 'https://api.themoviedb.org/3';
 export const POSTER_IMG = 'https://image.tmdb.org/t/p/original';
